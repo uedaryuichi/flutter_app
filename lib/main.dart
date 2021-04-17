93f7896d9cd6a31fa5573c5985804c3b0cce36d5
UedaRyuichi <uedaryuichi.sales@gmail.com>
2021-04-17 16:50:51 +0900

    providerパターンでモデルを作ってみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter でも',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home: MyHomePage(title: 'Flutter Demo ホーム Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the butこんなに:',
-            ),
-            Text(
-              'ラスト$_counter',
-              style: Theme.of(context).textTheme.headline2,
-            ),
-          ],
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('上ファ'),
+          ),
+          body: Consumer<MainModel>(
+            builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                  Text(
+                      model.uedaText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: Text('ボタン'),
+                      onPressed: () {
+                        model.changeUedaText();
+                      },
+                    ),
+                ],
+                ),
+              );
+            }
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.dangerous),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
